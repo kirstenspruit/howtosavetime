b6bc50bd254757dcfc60fdee243bb0dc52ad65f9
k-spruit <kirstenspruit@gmail.com>
2022-10-27 10:42:14 +0200

    MU performance

---
+++ essaying/at-MU/paste.js
@@ -0,0 +1,435 @@
+function showMoving1() {
+  var toShow = document.getElementById("moving1");
+  var button = document.getElementById("button1");
+  if (toShow.style.display === "block") {
+    toShow.style.display = "none";
+  } else {
+    toShow.style.display = "block";
+    button.style.display = "none";
+    document.body.style.backgroundColor = "white";
+  }
+}
+
+
+function showStill2() {
+  var prevMoving = document.getElementById("moving1");
+  var prevStill = document.getElementById("still1");
+  var toShow = document.getElementById("still2");
+  var button = document.getElementById("button2");
+  if (toShow.style.display === "block") {
+    toShow.style.display = "none";
+  } else {
+    prevMoving.style.display = "none";
+    prevStill.style.display = "block";
+    prevStill.style.color = "Gainsboro";
+    toShow.style.display = "block";
+    button.style.display = "none";
+    document.body.style.backgroundColor = "white";
+  }
+}
+
+function showMoving3() {
+  var prevStill = document.getElementById("still2");
+  var toShow = document.getElementById("moving3");
+  var button = document.getElementById("button3");
+  if (toShow.style.display === "block") {
+    toShow.style.display = "none";
+  } else {
+    prevStill.style.color = "Gainsboro";
+    toShow.style.display = "block";
+    button.style.display = "none";
+    document.body.style.backgroundColor = "white";
+  }
+}
+
+function showStill4() {
+  var prevMoving = document.getElementById("moving3");
+  var prevStill = document.getElementById("still3");
+  var toShow = document.getElementById("still4");
+  var button = document.getElementById("button4");
+  if (toShow.style.display === "block") {
+    toShow.style.display = "none";
+  } else {
+    prevMoving.style.display = "none";
+    prevStill.style.display = "block";
+    prevStill.style.color = "Gainsboro";
+    toShow.style.display = "block";
+    button.style.display = "none";
+    document.body.style.backgroundColor = "white";
+  }
+}
+
+function showStill5() {
+  var prevStill = document.getElementById("still4");
+  var toShow = document.getElementById("still5");
+  var button = document.getElementById("button5");
+  if (toShow.style.display === "block") {
+    toShow.style.display = "none";
+  } else {
+    prevStill.style.display = "block";
+    prevStill.style.color = "Gainsboro";
+    toShow.style.display = "block";
+    button.style.display = "none";
+    document.body.style.backgroundColor = "white";
+  }
+}
+
+function showStill6() {
+  var prevStill = document.getElementById("still5");
+  var toShow = document.getElementById("still6");
+  var button = document.getElementById("button6");
+  if (toShow.style.display === "block") {
+    toShow.style.display = "none";
+  } else {
+    prevStill.style.display = "block";
+    prevStill.style.color = "Gainsboro";
+    toShow.style.display = "block";
+    button.style.display = "none";
+    document.body.style.backgroundColor = "white";
+  }
+}
+
+function showStill7() {
+  var prevStill = document.getElementById("still6");
+  var toShow = document.getElementById("still7");
+  var button = document.getElementById("button7");
+  if (toShow.style.display === "block") {
+    toShow.style.display = "none";
+  } else {
+    prevStill.style.display = "block";
+    prevStill.style.color = "Gainsboro";
+    toShow.style.display = "block";
+    button.style.display = "none";
+    document.body.style.backgroundColor = "white";
+  }
+}
+
+function showStill8() {
+  var prevStill = document.getElementById("still7");
+  var toShow = document.getElementById("still8");
+  var button = document.getElementById("button8");
+  if (toShow.style.display === "block") {
+    toShow.style.display = "none";
+  } else {
+    prevStill.style.display = "block";
+    prevStill.style.color = "Gainsboro";
+    toShow.style.display = "block";
+    button.style.display = "none";
+    document.body.style.backgroundColor = "white";
+  }
+}
+
+function showStill9() {
+  var prevStill = document.getElementById("still8");
+  var toShow = document.getElementById("still9");
+  var button = document.getElementById("button9");
+  if (toShow.style.display === "block") {
+    toShow.style.display = "none";
+  } else {
+    prevStill.style.display = "block";
+    prevStill.style.color = "Gainsboro";
+    toShow.style.display = "block";
+    button.style.display = "none";
+    document.body.style.backgroundColor = "white";
+  }
+}
+
+function showStill10() {
+  var prevStill = document.getElementById("still9");
+  var toShow = document.getElementById("still10");
+  var button = document.getElementById("button10");
+  if (toShow.style.display === "block") {
+    toShow.style.display = "none";
+  } else {
+    prevStill.style.display = "block";
+    prevStill.style.color = "Gainsboro";
+    toShow.style.display = "block";
+    button.style.display = "none";
+    document.body.style.backgroundColor = "white";
+  }
+}
+
+function showStill11() {
+  var prevStill = document.getElementById("still10");
+  var toShow = document.getElementById("still11");
+  var button = document.getElementById("button11");
+  if (toShow.style.display === "block") {
+    toShow.style.display = "none";
+  } else {
+    prevStill.style.display = "block";
+    prevStill.style.color = "Gainsboro";
+    toShow.style.display = "block";
+    button.style.display = "none";
+    document.body.style.backgroundColor = "white";
+  }
+}
+
+function showStill12() {
+  var prevStill = document.getElementById("still11");
+  var toShow = document.getElementById("still12");
+  var button = document.getElementById("button12");
+  if (toShow.style.display === "block") {
+    toShow.style.display = "none";
+  } else {
+    prevStill.style.display = "block";
+    prevStill.style.color = "Gainsboro";
+    toShow.style.display = "block";
+    button.style.display = "none";
+    document.body.style.backgroundColor = "white";
+  }
+}
+
+function showStill13() {
+  var prevStill = document.getElementById("still12");
+  var toShow = document.getElementById("still13");
+  var button = document.getElementById("button13");
+  if (toShow.style.display === "block") {
+    toShow.style.display = "none";
+  } else {
+    prevStill.style.display = "block";
+    prevStill.style.color = "Gainsboro";
+    toShow.style.display = "block";
+    button.style.display = "none";
+    document.body.style.backgroundColor = "white";
+  }
+}
+
+function showStill14() {
+  var prevStill = document.getElementById("still13");
+  var toShow = document.getElementById("still14");
+  var button = document.getElementById("button14");
+  if (toShow.style.display === "block") {
+    toShow.style.display = "none";
+  } else {
+    prevStill.style.display = "block";
+    prevStill.style.color = "Gainsboro";
+    toShow.style.display = "block";
+    button.style.display = "none";
+    document.body.style.backgroundColor = "white";
+  }
+}
+
+function showStill15() {
+  var prevStill = document.getElementById("still14");
+  var toShow = document.getElementById("still15");
+  var button = document.getElementById("button15");
+  if (toShow.style.display === "block") {
+    toShow.style.display = "none";
+  } else {
+    prevStill.style.display = "block";
+    prevStill.style.color = "Gainsboro";
+    toShow.style.display = "block";
+    button.style.display = "none";
+    document.body.style.backgroundColor = "white";
+  }
+}
+
+function showStill16() {
+  var prevStill = document.getElementById("still15");
+  var toShow = document.getElementById("still16");
+  var button = document.getElementById("button16");
+  if (toShow.style.display === "block") {
+    toShow.style.display = "none";
+  } else {
+    prevStill.style.display = "block";
+    prevStill.style.color = "Gainsboro";
+    toShow.style.display = "block";
+    button.style.display = "none";
+    document.body.style.backgroundColor = "white";
+  }
+}
+
+function showStill17() {
+  var prevStill = document.getElementById("still16");
+  var toShow = document.getElementById("still17");
+  var button = document.getElementById("button17");
+  if (toShow.style.display === "block") {
+    toShow.style.display = "none";
+  } else {
+    prevStill.style.display = "block";
+    prevStill.style.color = "Gainsboro";
+    toShow.style.display = "block";
+    button.style.display = "none";
+    document.body.style.backgroundColor = "white";
+  }
+}
+
+function showStill18() {
+  var prevStill = document.getElementById("still17");
+  var toShow = document.getElementById("still18");
+  var button = document.getElementById("button18");
+  if (toShow.style.display === "block") {
+    toShow.style.display = "none";
+  } else {
+    prevStill.style.display = "block";
+    prevStill.style.color = "Gainsboro";
+    toShow.style.display = "block";
+    button.style.display = "none";
+    document.body.style.backgroundColor = "white";
+  }
+}
+
+function showStill19() {
+  var prevStill = document.getElementById("still18");
+  var toShow = document.getElementById("still19");
+  var button = document.getElementById("button19");
+  if (toShow.style.display === "block") {
+    toShow.style.display = "none";
+  } else {
+    prevStill.style.display = "block";
+    prevStill.style.color = "Gainsboro";
+    toShow.style.display = "block";
+    button.style.display = "none";
+    document.body.style.backgroundColor = "white";
+  }
+}
+
+function showStill20() {
+  var prevStill = document.getElementById("still19");
+  var toShow = document.getElementById("still20");
+  var button = document.getElementById("button20");
+  if (toShow.style.display === "block") {
+    toShow.style.display = "none";
+  } else {
+    prevStill.style.display = "block";
+    prevStill.style.color = "Gainsboro";
+    toShow.style.display = "block";
+    button.style.display = "none";
+    document.body.style.backgroundColor = "white";
+  }
+}
+
+function showStill21() {
+  var prevStill = document.getElementById("still20");
+  var toShow = document.getElementById("still21");
+  var button = document.getElementById("button21");
+  if (toShow.style.display === "block") {
+    toShow.style.display = "none";
+  } else {
+    prevStill.style.display = "block";
+    prevStill.style.color = "Gainsboro";
+    toShow.style.display = "block";
+    button.style.display = "none";
+    document.body.style.backgroundColor = "white";
+  }
+}
+
+function showStill22() {
+  var prevStill = document.getElementById("still21");
+  var toShow = document.getElementById("still22");
+  var button = document.getElementById("button22");
+  if (toShow.style.display === "block") {
+    toShow.style.display = "none";
+  } else {
+    prevStill.style.display = "block";
+    prevStill.style.color = "Gainsboro";
+    toShow.style.display = "block";
+    button.style.display = "none";
+    document.body.style.backgroundColor = "white";
+  }
+}
+
+function showStill23() {
+  var prevStill = document.getElementById("still22");
+  var toShow = document.getElementById("still23");
+  var button = document.getElementById("button23");
+  if (toShow.style.display === "block") {
+    toShow.style.display = "none";
+  } else {
+    prevStill.style.display = "block";
+    prevStill.style.color = "Gainsboro";
+    toShow.style.display = "block";
+    button.style.display = "none";
+    document.body.style.backgroundColor = "white";
+  }
+}
+
+function showStill24() {
+  var prevStill = document.getElementById("still23");
+  var toShow = document.getElementById("still24");
+  var button = document.getElementById("button24");
+  if (toShow.style.display === "block") {
+    toShow.style.display = "none";
+  } else {
+    prevStill.style.display = "block";
+    prevStill.style.color = "Gainsboro";
+    toShow.style.display = "block";
+    button.style.display = "none";
+    document.body.style.backgroundColor = "white";
+  }
+}
+
+function showStill25() {
+  var prevStill = document.getElementById("still24");
+  var toShow = document.getElementById("still25");
+  var button = document.getElementById("button25");
+  if (toShow.style.display === "block") {
+    toShow.style.display = "none";
+  } else {
+    prevStill.style.display = "block";
+    prevStill.style.color = "Gainsboro";
+    toShow.style.display = "block";
+    button.style.display = "none";
+    document.body.style.backgroundColor = "white";
+  }
+}
+
+function showStill26() {
+  var prevStill = document.getElementById("still25");
+  var toShow = document.getElementById("still26");
+  var button = document.getElementById("button26");
+  if (toShow.style.display === "block") {
+    toShow.style.display = "none";
+  } else {
+    prevStill.style.display = "block";
+    prevStill.style.color = "Gainsboro";
+    toShow.style.display = "block";
+    button.style.display = "none";
+    document.body.style.backgroundColor = "white";
+  }
+}
+
+function showMoving27() {
+  var prevStill = document.getElementById("still26");
+  var toShow = document.getElementById("moving27");
+  var button = document.getElementById("button27");
+  if (toShow.style.display === "block") {
+    toShow.style.display = "none";
+  } else {
+    prevStill.style.color = "Gainsboro";
+    toShow.style.display = "block";
+    button.style.display = "none";
+    document.body.style.backgroundColor = "white";
+  }
+}
+
+function showStill28() {
+  var prevMoving = document.getElementById("moving27");
+  var prevStill = document.getElementById("still27");
+  var toShow = document.getElementById("still28");
+  var button = document.getElementById("button28");
+  if (toShow.style.display === "block") {
+    toShow.style.display = "none";
+  } else {
+    prevMoving.style.display = "none";
+    prevStill.style.display = "block";
+    prevStill.style.color = "Gainsboro";
+    toShow.style.display = "block";
+    button.style.display = "none";
+    document.body.style.backgroundColor = "white";
+  }
+}
+
+function showMoving29() {
+  var prevStill = document.getElementById("still28");
+  var toShow = document.getElementById("moving29");
+  var button = document.getElementById("button29");
+  if (toShow.style.display === "block") {
+    toShow.style.display = "none";
+  } else {
+    prevStill.style.color = "Gainsboro";
+    toShow.style.display = "block";
+    button.style.display = "none";
+    document.body.style.backgroundColor = "white";
+  }
+}
